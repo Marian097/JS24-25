981a6447bf73ee9d37f7adc579cd8abdec0c10ae
Lungu Marian <141348340+Marian097@users.noreply.github.com>
2024-12-11 20:47:23 +0200

    JS

---
+++ curs1/js/ap4.js
@@ -0,0 +1,11 @@
+
+// Afiseaza mesaje in consola
+console.log("Mesaj demo");
+console.info("Mesaj doar informativ");
+console.error("Mesaj de eroare pentru a determina un anumit bug");
+// Afiseaza mesaje in consola 
+
+
+
+
+
