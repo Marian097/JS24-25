abec140f6b8a1be6c81b58451503a6462bf5fe26
Lungu Marian <141348340+Marian097@users.noreply.github.com>
2025-01-30 20:48:38 +0200

    jasmin(TDD)

---
+++ curs9/unittest/js/jasmin/spec/Math.js
@@ -0,0 +1,26 @@
+describe("helloWorld", () =>{
+    it("returns Hello World", () => {
+        var actual=helloWorld();
+        expect(actual).toBe("Hello World");
+    });
+    it("returns Hello", () =>{
+        var actual=helloWorld();
+        expect(actual).toContain("Hello");
+    });
+});
+
+describe("math sum", () => {
+    it("suma dintr 3 si 5", function(){
+        expect(sum(3, 5)).toBe(8);
+    })
+})
+
+
+describe("math sum2", () =>{
+    it("suma dintre 1.4 si 1.3", function(){
+        expect(sum(1.4, 1.3)).toEqual(2.7);
+    });
+    it ("suma dintre 0.1 si 0.7", function(){
+        expect(sum(0.1, 0.7)).toEqual(0.8);
+    });
+});
---
+++ curs9/unittest/js/jasmin/src/math.js
@@ -0,0 +1,19 @@
+function helloWorld()
+{
+    return "Hello World";
+};
+
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+function sum(a, b)
+{
+    a = a * 100;
+    b = b * 100;
+    let sum = a + b;
+    sum = sum / 100;
+    
+    return  Number(sum.toFixed(2));
+}
